6f9c369e1d90e48723f7a0d7e197e9fdc6f00cb7
Yonghui Chen <chyhfj@gmail.com>
2013-12-11 22:53:37 +0800

    add foundation prebuilt files

---
+++ Projects/Foundation/prebuilt/Foundation/GSMime.h
@@ -0,0 +1,2 @@
+#warning GSMime.h is now included using the path <GNUstepBase/GSMime.h>
+#include <GNUstepBase/GSMime.h>
---
+++ Projects/Foundation/prebuilt/Foundation/GSXML.h
@@ -0,0 +1,2 @@
+#warning GSXML.h is now included using the path <GNUstepBase/GSXML.h>
+#include <GNUstepBase/GSXML.h>
