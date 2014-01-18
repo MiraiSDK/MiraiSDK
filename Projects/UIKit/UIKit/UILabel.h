db506c7dc1cb5de9d473c98a9493c584209c7278
Yonghui Chen <chyhfj@gmail.com>
2014-01-19 07:19:11 +0800

    add UIButton class

--- Projects/UIKit/UIKit/UILabel.h
+++ Projects/UIKit/UIKit/UILabel.h
@@ -24,7 +24,7 @@
 //@property(nonatomic)        NSLineBreakMode    lineBreakMode;
 
 // the underlying attributed string drawn by the label, if set, the label ignores the properties above.
-//@property(nonatomic,copy)   NSAttributedString *attributedText NS_AVAILABLE_IOS(6_0);  // default is nil
+@property(nonatomic,copy)   NSAttributedString *attributedText;// NS_AVAILABLE_IOS(6_0);  // default is nil
 
 @property(nonatomic,retain)               UIColor *highlightedTextColor; // default is nil
 @property(nonatomic,getter=isHighlighted) BOOL     highlighted;          // default is NO
@@ -32,11 +32,9 @@
 @property(nonatomic,getter=isEnabled)                BOOL enabled;                 // default is YES. changes how the label is drawn
 @property(nonatomic) NSInteger numberOfLines;
 
-//@property(nonatomic) BOOL adjustsFontSizeToFitWidth;         // default is NO
-//@property(nonatomic) BOOL adjustsLetterSpacingToFitWidth NS_DEPRECATED_IOS(6_0,7_0); // deprecated - hand tune by using NSKernAttributeName to affect tracking
-//@property(nonatomic) CGFloat minimumFontSize NS_DEPRECATED_IOS(2_0, 6_0); // NOTE: deprecated - use minimumScaleFactor. default is 0.0
+@property(nonatomic) BOOL adjustsFontSizeToFitWidth;         // default is NO
 //@property(nonatomic) UIBaselineAdjustment baselineAdjustment; // default is UIBaselineAdjustmentAlignBaselines
-//@property(nonatomic) CGFloat minimumScaleFactor NS_AVAILABLE_IOS(6_0); // default is 0.0
+@property(nonatomic) CGFloat minimumScaleFactor;// NS_AVAILABLE_IOS(6_0); // default is 0.0
 
 // override points. can adjust rect before calling super.
 // label has default content mode of UIViewContentModeRedraw
@@ -47,6 +45,12 @@
 
 // Support for constraint-based layout (auto layout)
 // If nonzero, this is used when determining -intrinsicContentSize for multiline labels
-//@property(nonatomic) CGFloat preferredMaxLayoutWidth NS_AVAILABLE_IOS(6_0);
+@property(nonatomic) CGFloat preferredMaxLayoutWidth;// NS_AVAILABLE_IOS(6_0);
+
+
+#pragma mark - DEPRECATED
+@property(nonatomic) CGFloat minimumFontSize;// NS_DEPRECATED_IOS(2_0, 6_0); // NOTE: deprecated - use minimumScaleFactor. default is 0.0
+@property(nonatomic) BOOL adjustsLetterSpacingToFitWidth; //NS_DEPRECATED_IOS(6_0,7_0); // deprecated - hand tune by using NSKernAttributeName to affect tracking
+
 
 @end
