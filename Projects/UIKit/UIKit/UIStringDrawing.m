12df3c260076c1d33456cc796024e0e2145c0c6c
Yonghui Chen <chyhfj@gmail.com>
2014-01-20 03:28:08 +0800

    add UIStringDrawing empty methods

--- Projects/UIKit/UIKit/UIStringDrawing.m
+++ Projects/UIKit/UIKit/UIStringDrawing.m
@@ -7,13 +7,77 @@
 //
 
 #import "UIStringDrawing.h"
+#import "UIFont.h"
+
+#import <CoreGraphics/CoreGraphics.h>
+#import <CoreText/CoreText.h>
 
 NSString *const UITextAttributeFont = @"UITextAttributeFont";
 NSString *const UITextAttributeTextColor = @"UITextAttributeTextColor";
 NSString *const UITextAttributeTextShadowColor = @"UITextAttributeTextShadowColor";
 NSString *const UITextAttributeTextShadowOffset = @"UITextAttributeTextShadowOffset";
 
+
 @implementation NSString (UIStringDrawing)
 
+- (CGSize)sizeWithFont:(UIFont *)font
+{
+    return CGSizeZero;
+}
+
+- (CGSize)sizeWithFont:(UIFont *)font forWidth:(CGFloat)width lineBreakMode:(NSLineBreakMode)lineBreakMode
+{
+    return CGSizeZero;
+}
+
+- (CGSize)drawAtPoint:(CGPoint)point withFont:(UIFont *)font
+{
+    return CGSizeZero;
+}
+
+- (CGSize)drawAtPoint:(CGPoint)point forWidth:(CGFloat)width withFont:(UIFont *)font lineBreakMode:(NSLineBreakMode)lineBreakMode
+{
+    return CGSizeZero;
+}
+
+- (CGSize)sizeWithFont:(UIFont *)font constrainedToSize:(CGSize)size
+{
+    return CGSizeZero;
+}
+
+- (CGSize)sizeWithFont:(UIFont *)font constrainedToSize:(CGSize)size lineBreakMode:(NSLineBreakMode)lineBreakMode
+{
+    return CGSizeZero;
+}
+
+- (CGSize)drawInRect:(CGRect)rect withFont:(UIFont *)font
+{
+    return CGSizeZero;
+}
+
+- (CGSize)drawInRect:(CGRect)rect withFont:(UIFont *)font lineBreakMode:(NSLineBreakMode)lineBreakMode
+{
+    return CGSizeZero;
+}
+
+- (CGSize)drawInRect:(CGRect)rect withFont:(UIFont *)font lineBreakMode:(NSLineBreakMode)lineBreakMode alignment:(NSTextAlignment)alignment
+{
+    return CGSizeZero;
+}
+
+- (CGSize)sizeWithFont:(UIFont *)font minFontSize:(CGFloat)minFontSize actualFontSize:(CGFloat *)actualFontSize forWidth:(CGFloat)width lineBreakMode:(NSLineBreakMode)lineBreakMode
+{
+    return CGSizeZero;
+}
+
+- (CGSize)drawAtPoint:(CGPoint)point forWidth:(CGFloat)width withFont:(UIFont *)font fontSize:(CGFloat)fontSize lineBreakMode:(NSLineBreakMode)lineBreakMode baselineAdjustment:(UIBaselineAdjustment)baselineAdjustment
+{
+    return CGSizeZero;
+}
+
+- (CGSize)drawAtPoint:(CGPoint)point forWidth:(CGFloat)width withFont:(UIFont *)font minFontSize:(CGFloat)minFontSize actualFontSize:(CGFloat *)actualFontSize lineBreakMode:(NSLineBreakMode)lineBreakMode baselineAdjustment:(UIBaselineAdjustment)baselineAdjustment
+{
+    return CGSizeZero;
+}
 
 @end
