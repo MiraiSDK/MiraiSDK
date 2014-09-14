82dc1249e1710c99882938d665270d6a7c8882f8
Yonghui Chen <chyhfj@gmail.com>
2014-09-14 15:50:13 +0800

    support set screen size, fix layer bounds mask

--- DemoProject/MiraiTests/Basic/TNAppDelegate.m
+++ DemoProject/MiraiTests/Basic/TNAppDelegate.m
@@ -23,17 +23,24 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+#if __ANDROID__
+    [[UIScreen mainScreen] setScreenMode:UIScreenSizeModePad scale:0];
+#endif
     
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    CGRect screenBounds = [[UIScreen mainScreen] bounds];
+//    CGRect bounds = CGRectMake(0, 0, 768, 1024);
+//    bounds.origin.y = (screenBounds.size.height - bounds.size.height)/2;
+    self.window = [[UIWindow alloc] initWithFrame:screenBounds];
     self.window.backgroundColor = [UIColor blueColor];
-//    
+    self.window.clipsToBounds = YES;
+//
 //    UILabel *label = [[UILabel alloc] init];
 //    label.frame = CGRectMake(20, 20, 500, 500);
 //    label.layer.backgroundColor = [[UIColor greenColor] CGColor];
 //    label.text = [NSString stringWithFormat:@"Hello Label \n中文能支持\n日本語もOKです。"];
 //    [label setTextColor:[UIColor redColor]];
 //    [self.window addSubview:label];
-//    
+//
     
     TNViewController *vc = [[TNViewController alloc] initWithNibName:nil bundle:nil];
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
@@ -68,11 +75,171 @@
 
     NSLog(@"main bundle path:%@",[[NSBundle mainBundle] bundlePath]);
 
-    [self logPathSettings:NSUserDomainMask];
+    //[self logPathSettings:NSUserDomainMask];
     
+//    [self transformTest];
+    
+//    [self matrixTest];
+//    [self maskBoundsTest];
     return YES;
 }
 
+- (void)maskBoundsTest
+{
+//    CALayer *bg = [CALayer layer];
+//    bg.frame = CGRectMake(50, 50, 200, 200);
+//    bg.backgroundColor = [UIColor lightGrayColor].CGColor;
+//    [self.window.layer addSublayer:bg];
+
+    CALayer *parent = [CALayer layer];
+    parent.frame = CGRectMake(50, 50, 200, 200);
+    parent.backgroundColor = [UIColor redColor].CGColor;
+    parent.transform = CATransform3DMakeRotation(M_PI_4, 0, 0, 1);
+    parent.masksToBounds = YES;
+    
+    CALayer *child = [CALayer layer];
+    child.frame = CGRectMake(50, 50, 200, 200);
+    child.backgroundColor = [UIColor greenColor].CGColor;
+    child.masksToBounds = YES;
+
+    CALayer *child11 = [CALayer layer];
+    child11.frame = CGRectMake(50, 50, 200, 200);
+    child11.backgroundColor = [UIColor yellowColor].CGColor;
+    child11.masksToBounds = YES;
+    [child addSublayer:child11];
+    
+    CALayer *child1 = [CALayer layer];
+    child1.frame = CGRectMake(-150, -150, 200, 200);
+    child1.backgroundColor = [UIColor purpleColor].CGColor;
+    child1.masksToBounds = YES;
+    
+
+    
+    [parent addSublayer:child];
+    [parent addSublayer:child1];
+    
+    [self.window.layer addSublayer:parent];
+    
+    CALayer *p1 = [CALayer layer];
+    p1.frame = CGRectMake(350, 50, 200, 200);
+    p1.backgroundColor = [UIColor redColor].CGColor;
+    p1.masksToBounds = YES;
+    
+    CALayer *c1 = [CALayer layer];
+    c1.frame = CGRectMake(50, 50, 200, 200);
+    c1.backgroundColor = [UIColor greenColor].CGColor;
+    c1.masksToBounds = YES;
+    
+    CALayer *c2 = [CALayer layer];
+    c2.frame = CGRectMake(-150, -150, 200, 200);
+    c2.backgroundColor = [UIColor purpleColor].CGColor;
+    c2.masksToBounds = YES;
+
+    [p1 addSublayer:c1];
+    [p1 addSublayer:c2];
+    
+    [self.window.layer addSublayer:p1];
+}
+
+- (void)layerConvertTest
+{
+    //    UIView *yellowView = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 500, 500)];
+    //    yellowView.backgroundColor = [UIColor yellowColor];
+    //    [self.window addSubview:yellowView];
+    //
+    //    UIView *whiteview = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 400, 300)];
+    //    whiteview.backgroundColor = [UIColor whiteColor];
+    //    whiteview.transform = CGAffineTransformMakeRotation(M_PI_2);
+    //    [yellowView addSubview:whiteview];
+    //
+    //    UIView *greenview = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 200, 100)];
+    //    greenview.backgroundColor = [UIColor greenColor];
+    //    [whiteview addSubview:greenview];
+    //
+    //    UIView *grayview = [[UIView alloc] initWithFrame:CGRectMake(120, 550, 500, 300)];
+    //    grayview.backgroundColor = [UIColor grayColor];
+    //    [self.window addSubview:grayview];
+    //
+    //    CGRect r = [whiteview convertRect:whiteview.bounds toView:yellowView];
+    //    CGRect r2 = [whiteview convertRect:whiteview.bounds toView:grayview];
+    //
+    //    CGRect r3 = [greenview convertRect:greenview.bounds toView:yellowView];
+    //
+    //    NSLog(@"convert whiteview bounds:%@ to yellowView, result:%@",NSStringFromCGRect(whiteview.bounds),NSStringFromCGRect(r));
+    //    CGRect r1 = [whiteview.layer convertRect:whiteview.layer.bounds toLayer:yellowView.layer];
+    //    NSLog(@"convert whitelayer bounds:%@ to yellowlayer, result:%@",NSStringFromCGRect(whiteview.layer.bounds),NSStringFromCGRect(r1));
+    //
+    //    NSLog(@"convert whiteview bounds:%@ to grayview, result:%@",NSStringFromCGRect(whiteview.bounds),NSStringFromCGRect(r2));
+    //
+    //    NSLog(@"convert greenview bounds:%@ to yellowView, result:%@",NSStringFromCGRect(greenview.bounds),NSStringFromCGRect(r3));
+    
+    //    CGRect rrr = [greenview.layer convertRect:greenview.layer.bounds toLayer:yellowView.layer];
+    //    CGRect r = [v11.layer convertRect:v11.layer.bounds toLayer:v.layer];
+    //    NSLog(@"convert v11 layer bounds:%@ to v, result:%@",NSStringFromCGRect(greenview.layer.bounds),NSStringFromCGRect(rrr));
+
+}
+- (void)matrixTest
+{
+    CGAffineTransform t = CGAffineTransformMake(1.406250, 0, 0, 1.406250, -160.3125, -340.3125);
+    CGAffineTransform t1 = CGAffineTransformInvert(t);
+    
+    
+    CATransform3D t3d = CATransform3DMakeAffineTransform(t);
+    CATransform3D i3d = CATransform3DInvert(t3d);
+    CGAffineTransform t11 = CATransform3DGetAffineTransform(i3d);
+    
+    NSLog(@"t:%@",NSStringFromCGAffineTransform(t));
+    NSLog(@"t1:%@",NSStringFromCGAffineTransform(t1));
+    
+    NSLog(@"t11:%@",NSStringFromCGAffineTransform(t11));
+
+    NSLog(@"concat:%@",NSStringFromCGAffineTransform(CGAffineTransformConcat(t, t1)));
+}
+
+NSString *NSStringFromCATransform3D(CATransform3D t)
+{
+    return [NSString stringWithFormat:@"{%.8f %.8f %.8f %.8f,\n%.8f %.8f %.8f %.8f,\n%.8f %.8f %.8f %.8f,\n%.8f %.8f %.8f %.8f}",t.m11,t.m12,t.m13,t.m14,
+            t.m21,t.m22,t.m23,t.m24,
+            t.m31,t.m32,t.m33,t.m34,
+            t.m41,t.m42,t.m43,t.m44];
+}
+- (void)transformTest
+{
+    UIView *whiteView1 = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 400, 300)];
+    whiteView1.transform = CGAffineTransformMakeRotation(M_PI_2);
+
+    CALayer *child = whiteView1.layer;
+
+    CGPoint parentAnchorFix = CGPointMake(250, 250);
+
+    CGPoint childAnchorFix = CGPointMake(200, 150);
+    CGPoint anchorPosition = CGPointMake(child.position.x - parentAnchorFix.x,
+                                         child.position.y - parentAnchorFix.y);
+    
+    CATransform3D anchorTranslate = CATransform3DMakeTranslation(anchorPosition.x, anchorPosition.y, 0);
+    CATransform3D childTransform = child.transform;
+    
+    CATransform3D anchorTransform = CATransform3DConcat(childTransform,anchorTranslate);
+    CGAffineTransform cg_anchorTransform = CATransform3DGetAffineTransform(anchorTransform);
+
+    CGPoint child_view_coord_point = CGPointZero;
+    CGPoint child_anchor_coord_point = CGPointMake(child_view_coord_point.x - childAnchorFix.x,
+                                                   child_view_coord_point.y - childAnchorFix.y);
+    
+    CGPoint converted_anchor_point = CGPointApplyAffineTransform(child_anchor_coord_point, cg_anchorTransform);
+    CGPoint converted_view_point = CGPointMake(converted_anchor_point.x + parentAnchorFix.x,
+                                               converted_anchor_point.y + parentAnchorFix.y);
+    
+    CGRect r = CGRectMake(0, 0, 50, 50);
+    CGRect ar = CGRectOffset(r, -childAnchorFix.x, -childAnchorFix.y);
+    CGRect car = CGRectApplyAffineTransform(ar, cg_anchorTransform);
+    CGRect cvr = CGRectOffset(car, parentAnchorFix.x, parentAnchorFix.y);
+    
+    NSLog(@"3d transform:%@",NSStringFromCATransform3D(anchorTransform));
+    NSLog(@"affine transform:%@",NSStringFromCGAffineTransform(cg_anchorTransform));
+    NSLog(@"vp:%@ ap:%@ cap:%@ cvp:%@",NSStringFromCGPoint(child_view_coord_point),NSStringFromCGPoint(child_anchor_coord_point),NSStringFromCGPoint(converted_anchor_point),NSStringFromCGPoint(converted_view_point));
+    NSLog(@"vr:%@ ar:%@ car:%@ cvr:%@",NSStringFromCGRect(r),NSStringFromCGRect(ar),NSStringFromCGRect(car),NSStringFromCGRect(cvr));
+}
 - (void)logPathSettings:(NSSearchPathDomainMask)mask
 {
     NSLog(@"domain:%d",mask);
