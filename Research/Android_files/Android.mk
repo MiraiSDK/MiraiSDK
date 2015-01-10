9b4d45b67b3e256de38dadc1fe4a4504ec169aa5
Yonghui Chen <chyhfj@gmail.com>
2015-01-10 21:39:39 +0800

    add documentations

---
+++ Research/Android_files/Android.mk
@@ -0,0 +1,80 @@
+
+LOCAL_PATH := $(call my-dir)
+LOCAL_EXPORT_CFLAGS += -g
+
+TN_LIBRARY_PATH := /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/Android18.sdk/usr/lib
+
+# system library
+include $(CLEAR_VARS)
+LOCAL_MODULE    := dispatch
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libdispatch.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := objc
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libobjc.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := gnustl_shared
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libgnustl_shared.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := gnustep-base
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libgnustep-base.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := TNJavaHelper
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libTNJavaHelper.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := gnustep-corebase
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libgnustep-corebase.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := CoreGraphics
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libCoreGraphics.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := OpenGLES
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libOpenGLES.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := CoreText
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libCoreText.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := QuartzCore
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libQuartzCore.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := ObjectiveZip
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libObjectiveZip.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := UIKit
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libUIKit.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := MediaPlayer
+LOCAL_SRC_FILES := $(TN_LIBRARY_PATH)/libMediaPlayer.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+# Application
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libDemo-Android
+LOCAL_SRC_FILES := libDemo-Android.so
+include $(PREBUILT_SHARED_LIBRARY)
+
--- Research/how_to_build_android_target.md
+++ Research/how_to_build_android_target.md
@@ -1,9 +1,40 @@
-#How To Build Android Target
+#How To Build Android Target Manually
 
+
+##Xcode part
 1. add static library target
 2. copy and add Android.xcconfig to project and set it as configuration
 3. change Mach-O Type to dynamic library
 4. remove Other Linker Flags override
-5. remove Installation BUild Products Location override
+5. Link UIKit.framework
+5. remove Installation Build Products Location override
 6. change Skit Install to No
+7. add lanucher class 
+8. add ``[[UIScreen mainScreen] setScreenMode:UIScreenSizeModePad scale:0];``
+9. create UIWindow and rootViewController
+10. add ndk-build script
+
+
+##Android part
+
+### 1. create android project
+
+run command in android folder :
+
+```
+android create project --target 1 --name Demo --path ./ --activity MainActivity --package org.tiny4.Demo
+```
+
+### 2. create jni/Android.mk
+
+see [Android.mk](Android_files/Android.mk)
+
+
+###3. copy CocoaActivity folder
+
+###4. modify java file, 
+	
+1. add ``import org.tiny4.CocoaActivity.CocoaActivity; ``
+2. change super class to ``CocoaActivity``
+3. load library
 
